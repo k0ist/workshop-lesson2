afb308bc7de0d15f6531beefb3d584648969bb83
koist <arseniy.ershov.1206@mail.ru>
2024-12-03 16:55:45 +0300

    #4 commit

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-a = list(map(int, input().split()))
-print(max(a))
+def find_max(a):
+    return max(a)
+
+
+def main():
+    length = int(input("Enter the length of your array: "))
+    a = [0] * length
+    for i in range(length):
+        a[i] = int(input())
+    print("Maximum of array is", find_max(a))
+
+
+main()
